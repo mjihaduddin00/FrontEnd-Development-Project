f63a0a9d46aa9ffb3feaa45d4dcdc7f412745dc4
Jonathan Sarmiento <john.sarmiento@me.com>
2020-05-21 15:56:04 -0500

    Many Edits..
    HTML
    =====
    
    -Added Containers for each section let me know if this works..
    
    - Added date and time beacuse why not...
    
    - Centered Logo i found on google for DreamTeam
    
    CSS
    ===
    
    -Turned off all CSS for now & created new lines of code, went with a darker color since its easier on the eye..most news pages are bright and i personally cant stand another one lol
    
    JS
    ==
    
    - Added a section for the Date and Time
    
    General
    =======
    
    Ill keep making edits to teh HTML, i added a font from google called Source, and Cleaned up the code/organized hope this works.

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//======================= Time & Date =======================//
+
+$(document).ready(function() {
+
+    var dateAndYear = moment().format('MMMM Do, YYYY');
+    $('#currentDay').html(dateAndYear);
+    console.log(dateAndYear);
+
+    var timeHMS = moment().format('h:mm a');
+    $('#currentHour').html(timeHMS);
+    console.log(timeHMS);
+
+});
