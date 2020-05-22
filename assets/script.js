ff09b9a955ae5edc45f066a1fe3c97d380d5a45e
Nathan Hall <Nashing1@gmail.com>
2020-05-22 17:25:19 -0500

    updated covid tracker
    dropped code into covid block in index. dropped script codes into covid section. added another jquery script loader into scripts section of index. could possibly delete first jquery load, need to investigate

--- assets/script.js
+++ assets/script.js
@@ -13,4 +13,45 @@ $(document).ready(function() {
 });
 
 
+//======================= Covid Tracker =======================//
+
+$(document).ready(function(){
+
+    function init(){
+        var url = "https://api.covid19api.com/summary"
+
+        var data = ''
+
+        $.get(url,function(data){
+            console.log(data.Global)
+
+            data = `
+            
+            <td>${data.Global.TotalConfirmed}</td>
+            <td>${data.Global.TotalDeaths}</td>
+            <td>${data.Global.TotalRecovered}</td>
+
+            `
+
+            $("#data").html(data)
+        })
+    }
+
+
+    function refreshData(){
+        clearData();
+        init();
+    }
+
+    function clearData(){
+        $("#data").empty()
+    }
+
+    init();
+
+    $('#refreshCovid').on('click', function(){
+        refreshData();
+    })
+});
+
 //======================= AP NAME AND CODE BELOW =======================//
