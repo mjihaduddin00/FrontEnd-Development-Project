9019a67423e5eb4aa6d9fd5b877c3bc875b6b0c1
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2020-05-28 02:26:41 -0500

    Edits
    Target Blank added to the More Stories button, renamed the title of the newsindex.html to Latest Stories, and tried troubleshooting Weather API.

--- assets/script.js
+++ assets/script.js
@@ -60,51 +60,51 @@ $(document).ready(function() {
         key: "54e6965a6aef2f55b313d4ee2c389024",
         base: "https://api.openweathermap.org/data/2.5/"
     }
-
+    
     const searchbox = document.querySelector('.search-box');
     searchbox.addEventListener('keypress', setQuery);
-
+    
     function setQuery(evt) {
-        if (evt.keyCode == 13) {
-            getResults(searchbox.value);
-        }
+      if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+      }
     }
-
-    function getResults(query) {
-        fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-            .then(weather => {
-                return weather.json();
-            }).then(displayResults);
+    
+    function getResults (query) {
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(weather => {
+          return weather.json();
+        }).then(displayResults);
     }
-
-    function displayResults(weather) {
-        let city = document.querySelector('.location .city');
-        city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-        let now = new Date();
-        let date = document.querySelector('.location .date');
-        date.innerText = dateBuilder(now);
-
-        let temp = document.querySelector('.current .temp');
-        temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
-
-        let weather_el = document.querySelector('.current .weather');
-        weather_el.innerText = weather.weather[0].main;
-
-        let hilow = document.querySelector('.hi-low');
-        hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
+    
+    function displayResults (weather) {
+      let city = document.querySelector('.location .city');
+      city.innerText = `${weather.name}, ${weather.sys.country}`;
+    
+      let now = new Date();
+      let date = document.querySelector('.location .date');
+      date.innerText = dateBuilder(now);
+    
+      let temp = document.querySelector('.current .temp');
+      temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
+    
+      let weather_el = document.querySelector('.current .weather');
+      weather_el.innerText = weather.weather[0].main;
+    
+      let hilow = document.querySelector('.hi-low');
+      hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
     }
-
-    function dateBuilder(d) {
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-        let day = days[d.getDay()];
-        let date = d.getDate();
-        let month = months[d.getMonth()];
-        let year = d.getFullYear();
-
-        return `${day} ${date} ${month} ${year}`;
+    
+    function dateBuilder (d) {
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+    
+      return `${day} ${date} ${month} ${year}`;
     }
 });
 
